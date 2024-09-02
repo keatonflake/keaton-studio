9415a86e92b28c60ddba5bf2b9348f85d75b7173
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2024-09-01 22:06:37 -0600

    added fix for production

--- lib/fetchProjects.ts
+++ lib/fetchProjects.ts
@@ -16,3 +16,19 @@ export async function fetchProjects() {
         await client.close();
     }
 }
+
+// import { fetchProjects } from "../../lib/fetchProjects"; // Update the path as necessary
+
+// export default async function ProjectsPage() {
+//   const projects = await fetchProjects();
+
+//   console.log(projects); // Logs the data to the server console
+
+//   return (
+//     <div>
+//       <h1>Projects</h1>
+//       <pre>{JSON.stringify(projects, null, 2)}</pre>
+//     </div>
+//   );
+// }
+
