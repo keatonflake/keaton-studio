432b296d9a44e96599422dbed44f0ad1ecc65d25
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2024-09-01 23:15:24 -0600

    last try to resolve reneder build error for tonight

--- lib/fetchProjects.ts
+++ lib/fetchProjects.ts
@@ -1,22 +1,30 @@
-const { MongoClient } = require('mongodb');
+import connectDB from "./mongodb";
+import { MongoClient } from 'mongodb';
+
 const uri = process.env.MONGODB_URI || '';
 const client = new MongoClient(uri);
 
 export async function fetchProjects() {
     try {
+        await connectDB(); // Ensure the connection is established
+
+        // Connect to the database using the native MongoClient
         await client.connect();
         const database = client.db();
         const projectsCollection = database.collection('projects');
         const projects = await projectsCollection.find({}).toArray();
+
         return projects;
     } catch (error) {
         console.error(`Error: ${(error as Error).message}`);
-        process.exit(1);
+        throw error; // Optionally throw the error to handle it in the calling function
     }
-    // finally {
-    //     await client.close();
-    // }
 }
+
+
+// finally {
+//     await client.close();
+// }
 
 // import { fetchProjects } from "../../lib/fetchProjects"; // Update the path as necessary
 
--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -2,9 +2,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async (): Promise<void> => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI || '');
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        if (mongoose.connection.readyState >= 1) return; // If already connected
 
+        await mongoose.connect(process.env.MONGODB_URI || '');
+        console.log(`MongoDB Connected: ${mongoose.connection.host}`);
     } catch (error) {
         console.error(`Error: ${(error as Error).message}`);
         process.exit(1);
