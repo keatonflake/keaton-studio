34f090244df6cbe8768e23ae53ab32540fb92069
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2024-09-01 22:04:17 -0600

    added image optomization

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,14 +1,14 @@
-import { fetchProjects } from "../../lib/fetchProjects"; // Update the path as necessary
+// import { fetchProjects } from "../../lib/fetchProjects"; // Update the path as necessary
 
-export default async function ProjectsPage() {
-  const projects = await fetchProjects();
+// export default async function ProjectsPage() {
+//   const projects = await fetchProjects();
 
-  console.log(projects); // Logs the data to the server console
+//   console.log(projects); // Logs the data to the server console
 
-  return (
-    <div>
-      <h1>Projects</h1>
-      <pre>{JSON.stringify(projects, null, 2)}</pre>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>Projects</h1>
+//       <pre>{JSON.stringify(projects, null, 2)}</pre>
+//     </div>
+//   );
+// }
